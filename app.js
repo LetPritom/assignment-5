a57996f3d6ebec6f5b97390b19592cf7d7268c96
LetPritom <pritom.royjess@gmail.com>
2025-09-02 02:16:35 +0600

    added my all  functional file

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log(`baincod`)
+// first logic
+
+const hearts = document.querySelectorAll(`.love`);
+const para = document.getElementsByClassName(`nav-para`);
+const coin = document.getElementsByClassName(`taka`);
+const increase = document.getElementById(`gain`);
+let count = 0 ;
+
+// first logic
+
+for (const heart of hearts) {
+    heart.addEventListener(`click`, function () {
+        count++ ;
+        increase.innerText = count ;
+    })
+}
